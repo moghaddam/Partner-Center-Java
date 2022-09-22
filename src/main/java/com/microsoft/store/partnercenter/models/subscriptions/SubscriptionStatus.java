34878b2185c0c419c0b69966cec2b269c64c31b1
Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>
2022-09-22 14:01:41 +0200

    Add "expired" to the SubscriptionStatus enumeration
    Signed-off-by: Ehsan Zaery Moghaddam <zaerymoghaddam@gmail.com>

--- src/main/java/com/microsoft/store/partnercenter/models/subscriptions/SubscriptionStatus.java
+++ src/main/java/com/microsoft/store/partnercenter/models/subscriptions/SubscriptionStatus.java
@@ -25,7 +25,17 @@ public enum SubscriptionStatus
     /**
      * Subscription state: Deleted
      */
-    DELETED("deleted");
+    DELETED("deleted"),
+
+    /**
+     * Subscription state: Pending
+     */
+    PENDING("pending"),
+
+    /**
+     * Subscription state: Expired
+     */
+    EXPIRED("expired");
 
     private final String value;
 
